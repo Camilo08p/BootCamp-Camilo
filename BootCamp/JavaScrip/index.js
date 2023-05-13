74396a2fa798396365e19a0460302d0bcdf67ceb
Camilo <camilo17361@outlook.com>
2023-05-12 21:48:57 -0500

    agregue flecha de navegacion, archivos java inicial

---
+++ BootCamp/JavaScrip/index.js
@@ -0,0 +1,37 @@
+// var texto = "hola esto es un texto de prueba jv "
+// var texto2 =  "y una variable"
+// console.log(texto+texto2)
+// var numero = 2
+// var numero2 =5
+// var resultado =numero * numero2
+// console.log (resultado)
+// console.log (numero+numero2)
+
+// var prueba =45
+// console.log (prueba)
+// prueba = "texto"
+// console.log(prueba)
+
+// let nuevaVariable= "nombre"
+// console.log (nuevaVariable)
+// nuevaVariable = "apellido"
+// console.log (nuevaVariable)
+
+// const myName = "Camilo"
+// console.log(myName)
+// // myName = "Daniel"
+// // console.log (myName)
+
+// function sumar(){
+//     let num = 3300.45
+//     let num2 = 99
+//     console.log (num + num2)
+// }
+
+function sumar(num1, num2){
+    console.log(num1 + num2)
+}
+
+sumar(10, 5)
+sumar(100, 120)
+sumar(5, 7)
