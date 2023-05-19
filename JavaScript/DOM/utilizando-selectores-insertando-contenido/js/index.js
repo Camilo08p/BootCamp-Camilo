399ddae2fbea9b28506a77e896bc09ba0964cfcb
Camilo <camilo17361@outlook.com>
2023-05-18 22:08:32 -0500

    formulario trabajado en clase con JS

--- JavaScript/DOM/utilizando-selectores-insertando-contenido/js/index.js
+++ JavaScript/DOM/utilizando-selectores-insertando-contenido/js/index.js
@@ -3,9 +3,12 @@ const main = document.createElement ('main')
 const span = document.createElement ('span')
 
 body.style.height = '100vh'
+// inserta codigo HTLM
 body.innerHTML = "<h1>Soy un h1 generado en js</h1>"
 body.innerHTML = "<h1>hola</h1>"
 
+//inseta texto
 span.innerText = "hola soy un span"
+//añadiendo span al body
 body.appendChild (main)
 main.appendChild (span)
