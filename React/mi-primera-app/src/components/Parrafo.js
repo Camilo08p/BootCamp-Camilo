888d1fe19f28bb011a8b98dbbfe4010fcfcd85cc
Camilo <camilo17361@outlook.com>
2023-05-24 14:46:51 -0500

    agregue app con vite

---
+++ React/mi-primera-app/src/components/Parrafo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Parrafo = (props) => {
+
+    return (
+        <div>
+        <p>Hola, vengo desde Parrafo.JS y {props.nombre} </p>
+        <span>y yo soy un span</span>
+        </div>
+
+    )
+}
+
+export default Parrafo
